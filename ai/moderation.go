5e3ee5713612f14fd25602c55a2306d7b9b94bbf
koenno <8572607+koenno@users.noreply.github.com>
2024-01-28 22:39:22 +0100

    moderator constructor

--- ai/moderation.go
+++ ai/moderation.go
@@ -11,6 +11,12 @@ type Moderator struct {
 	client *openai.Client
 }
 
+func NewModerator(client *openai.Client) *Moderator {
+	return &Moderator{
+		client: client,
+	}
+}
+
 // Moderate returns true if moderation is required and the given entry does not fullfil openai usage policy
 func (m *Moderator) Moderate(ctx context.Context, entry string) (bool, error) {
 	req := openai.ModerationRequest{
